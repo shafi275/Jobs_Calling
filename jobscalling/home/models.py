e9293b3d77f00ece37d8b9c98647b11aa07f9e82
shafi <shariyarshafi27@gmail.com>
2025-09-25 13:02:26 +0600

    company model

--- jobscalling/home/models.py
+++ jobscalling/home/models.py
@@ -9,3 +9,16 @@ class CandidateProfile(models.Model):
 
     def __str__(self):
         return self.full_name
+    
+class CompanyProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=200)
+    industry = models.CharField(max_length=100)
+    company_size = models.CharField(max_length=50)
+    contact_person = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20)
+    website = models.URLField(blank=True, null=True)
+    agree_terms = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.company_name
